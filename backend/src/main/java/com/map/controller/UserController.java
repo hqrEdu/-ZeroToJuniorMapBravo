cabd9f8b68e9ded1323b9b46fdcc6d329bec5af3
Mateusz <88348890+MateuszLesisz@users.noreply.github.com>
2023-03-26 22:15:49 +0200

    add request mapping

--- backend/src/main/java/com/map/controller/UserController.java
+++ backend/src/main/java/com/map/controller/UserController.java
@@ -5,8 +5,10 @@ import com.map.model.User;
 import com.map.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/api/users")
@@ -15,7 +17,7 @@ public class UserController {
 
     private final UserService userService;
 
-    @PostMapping("/create")
+    @PostMapping()
     ResponseEntity<User> addUser(@RequestBody UserDto userDto) {
         return ResponseEntity.accepted().body(userService.createUser(userDto));
     }
